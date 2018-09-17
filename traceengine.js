e89e27e8965f5ff7f9acc046023f7edc78ae2f00
jean3xw <jean3xw@hotmail.com>
2018-09-18 01:07:22 +0200

    Update traceengine.js

--- traceengine.js
+++ traceengine.js
@@ -1,12 +1,13 @@
 /** 
 TraceEngine
+never used stuff, see canvasmachine.js & dom2d.js
 */
 function newClass(n, t,d){
 	return { name:n, type:t, domain:[d], mode:'DOM', listViews:{dom2D:new DOM2D()}};
 }
 class Trace extends functrace{
 	constructor(tpe,dom){
-		super('Trace','classTrace#DawObject','Trace']);
+		super('Trace','classTrace#DrawObject','Trace']);
 	}
 	trace2DWith(md==){
 		md=='DOM'
